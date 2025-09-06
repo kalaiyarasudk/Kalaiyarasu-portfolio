696909da9541261a80f61c76b9461254803896f5
kalaiyarasudk <kalaiyarasu.dk@gmail.com>
2025-09-06 12:54:00 +0530

    Update images test

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   trailingSlash: true,
-//   images: {
-//     unoptimized: true,
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-// }
-
-// export default nextConfig
-
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  basePath: isProd ? '/p-5' : '',
-  assetPrefix: isProd ? '/p-5/' : '',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+export default nextConfig
+
+// const isProd = process.env.NODE_ENV === 'production';
+
+// module.exports = {
+//   basePath: isProd ? '/p-5' : '',
+//   assetPrefix: isProd ? '/p-5/' : '',
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true,
+// };
